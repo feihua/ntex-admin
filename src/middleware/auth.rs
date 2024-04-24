1a16e12e47c60620af22dfee512d27978329d505
liufeihua <1002219331@qq.com>
2024-04-24 17:09:40 +0800

    ntex1.x的中间件升级middleware

--- src/middleware/auth.rs
+++ src/middleware/auth.rs
@@ -1,7 +1,6 @@
 use log::info;
 use ntex::http::header;
 use ntex::service::{Middleware, Service, ServiceCtx};
-use ntex::util::BoxFuture;
 use ntex::web;
 use ntex::web::HttpResponse;
 
@@ -34,43 +33,40 @@ impl<S, Err> Service<web::WebRequest<Err>> for JwtAuthMiddleware<S>
 {
     type Response = web::WebResponse;
     type Error = web::Error;
-    type Future<'f> = BoxFuture<'f, Result<Self::Response, Self::Error>> where Self: 'f;
 
     ntex::forward_poll_ready!(service);
 
-    fn call<'a>(&'a self, req: web::WebRequest<Err>, ctx: ServiceCtx<'a, Self>) -> Self::Future<'_> {
+    async fn call(&self, req: web::WebRequest<Err>, ctx: ServiceCtx<'_, Self>) -> Result<Self::Response, Self::Error> {
         let path = req.path().to_string();
         info!("Hi from start. You requested path: {}", path);
         let header_value = header::HeaderValue::from_str("").unwrap();
         let authorization = req.headers().get("Authorization").unwrap_or(&header_value).to_str().unwrap().to_string();
 
-        Box::pin(async move {
-            if path.contains("login") {
-                return Ok(ctx.clone().call(&self.service, req).await?);
+        if path.contains("login") {
+            return Ok(ctx.clone().call(&self.service, req).await?);
+        }
+
+        if authorization.len() <= 0 {
+            return Ok(req.into_response(HttpResponse::Ok().json(&err_result_msg("token不能为空".to_string()))));
+        }
+
+        let jwt_token = match JWTToken::verify("123", &authorization.replace("Bearer ", "")) {
+            Ok(j_token) => j_token,
+            Result::Err(err) => {
+                let er = match err {
+                    WhoUnfollowedError::JwtTokenError(s) => { s }
+                    _ => "no math error".to_string()
+                };
+                log::error!("You requested path: {}, token is err: {}", path, er);
+                return Ok(req.into_response(HttpResponse::Ok().json(&err_result_msg(er))));
             }
+        };
 
-            if authorization.len() <= 0 {
-                return Ok(req.into_response(HttpResponse::Ok().json(&err_result_msg("token不能为空".to_string()))));
-            }
-
-            let jwt_token = match JWTToken::verify("123", &authorization.replace("Bearer ", "")) {
-                Ok(j_token) => j_token,
-                Result::Err(err) => {
-                    let er = match err {
-                        WhoUnfollowedError::JwtTokenError(s) => { s }
-                        _ => "no math error".to_string()
-                    };
-                    log::error!("You requested path: {}, token is err: {}", path, er);
-                    return Ok(req.into_response(HttpResponse::Ok().json(&err_result_msg(er))));
-                }
-            };
-
-            return if jwt_token.permissions.contains(&path) {
-                Ok(ctx.call(&self.service, req).await?)
-            } else {
-                log::error!("You has no permissions requested path: {:?}", &path);
-                Ok(req.into_response(HttpResponse::Ok().json(&err_result_msg("无权限访问".to_string()))))
-            };
-        })
+        return if jwt_token.permissions.contains(&path) {
+            Ok(ctx.call(&self.service, req).await?)
+        } else {
+            log::error!("You has no permissions requested path: {:?}", &path);
+            Ok(req.into_response(HttpResponse::Ok().json(&err_result_msg("无权限访问".to_string()))))
+        };
     }
 }
